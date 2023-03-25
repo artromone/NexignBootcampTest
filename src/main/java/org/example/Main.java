0de8718be95cfcd35667ad8cacdd689855db1920
Artem <artrom170@gmail.com>
2023-03-25 20:50:24 +0300

    init commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        printReport();
+    }
+    private static void printReport() {
+        System.out.println("Hello world!");
+    }
+}
